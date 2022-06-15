34ac3d6e4785696c163397fe6ab559c3616ed265
cdt96 <cody.theroux3@gmail.com>
2022-06-15 19:11:03 -0400

    wrote out my requires in index.js and wrote out my writefile function

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const inquirer = require("inquirer")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const writeFile = (fileContent) => {
+    fs.writeFile("./output/index.html", fileContent, (err) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Your team profile file has been generated!');
+		}
+	});
+};
