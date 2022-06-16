dcdaa4f0ce19283544e54a63f3630a26d15b8200
cdt96 <cody.theroux3@gmail.com>
2022-06-16 15:37:31 -0400

    stopping point for right now

--- utils/generatehtml.js
+++ utils/generatehtml.js
@@ -23,9 +23,9 @@ const template = `<!DOCTYPE html>
 </head>
 
 <body>
-    
+     
       <div class="card bg-light mb-3" style="max-width: 18rem;">
-        <div class="card-header">Header</div>
+        <div class="card-header">${Manager.data.getRole}</div>
         <div class="card-body">
           <h5 class="card-title">Light card title</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -33,33 +33,25 @@ const template = `<!DOCTYPE html>
       </div>
 
       <div class="card bg-light mb-3" style="max-width: 18rem;">
-        <div class="card-header">Header</div>
+        <div class="card-header">${Engineer.data.getRole}</div>
         <div class="card-body">
           <h5 class="card-title">Light card title</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
       </div>
-      
+
       <div class="card bg-light mb-3" style="max-width: 18rem;">
-        <div class="card-header">Header</div>
+        <div class="card-header">${Intern.data.getRole}</div>
         <div class="card-body">
           <h5 class="card-title">Light card title</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
       </div>
-      
-      <div class="card bg-light mb-3" style="max-width: 18rem;">
-        <div class="card-header">Header</div>
-        <div class="card-body">
-          <h5 class="card-title">Light card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-      
-      
+      }
+    
 </body>
 
 </html>`
 writeToFile(template);
-}
+    }
 module.exports = generatehtml;
