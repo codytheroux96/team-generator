2e13a450c7e72efe779f755079cf5f44ba6c224d
cdt96 <cody.theroux3@gmail.com>
2022-06-15 21:13:41 -0400

    added.js to all my requires on lines 3-6 on index.js and to line 1 on engineer.js, intern.js and manager.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const Employee = require("./employee");
-const {Engineer, engineerQuestionsArray} = require("./lib/engineer");
-const {Intern, internQuestionsArray} = require("./lib/intern");
-const {Manager, managerQuestionsArray}= require("./lib/manager");
+const Employee = require("./employee.js");
+const {Engineer, engineerQuestionsArray} = require("./lib/engineer.js");
+const {Intern, internQuestionsArray} = require("./lib/intern.js");
+const {Manager, managerQuestionsArray}= require("./lib/manager.js");
 
 
 
