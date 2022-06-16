8db2553b4c545c9f869402e67487110315c1eb74
cdt96 <cody.theroux3@gmail.com>
2022-06-16 09:11:16 -0400

    wrote out an init function starting the user off with the managerquestionsarray

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const {Engineer, engineerQuestionsArray} = require("./lib/engineer.js");
 const {Intern, internQuestionsArray} = require("./lib/intern.js");
 const {Manager, managerQuestionsArray}= require("./lib/manager.js");
 
-
+const init = () => {managerQuestions};
+const managerQuestions = () => {
+    inquirer
+    .prompt(managerQuestionsArray)
+    .then((data) => {
+        data = new Manager (data.name, data.id, data.office, data.email)
+    })
+};
 
 
 
