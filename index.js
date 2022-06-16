a5c776cea1437f1953539f70ea36d3937708a4b9
cdt96 <cody.theroux3@gmail.com>
2022-06-16 14:23:48 -0400

    made a generatehtml.js and required it on index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const Employee = require("./lib/employee.js");
+const generatehtml = require("./output/generatehtml.js")
 const { Engineer, engineerQuestionsArray } = require("./lib/engineer.js");
 const { Intern, internQuestionsArray } = require("./lib/intern.js");
 const { Manager, managerQuestionsArray } = require("./lib/manager.js");
