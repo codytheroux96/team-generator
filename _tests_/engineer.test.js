598d2f0c4386027b11aae43660f4d9a6ed00aeaa
cdt96 <cody.theroux3@gmail.com>
2022-06-18 14:34:57 -0400

    made tests for employee and engineer

--- _tests_/engineer.test.js
+++ _tests_/engineer.test.js
@@ -1,1 +1,20 @@
 const Engineer = require("../lib/engineer");
+
+describe("Engineer")
+it("Creates new engineer", () => {
+    const engineer = new Engineer("Your engineer", "ab12", "your@engineer.com", "your-engineer");
+    expect(engineer.name).toBe("Your engineer");
+    expect(engineer.id).toEqual(expect.any(String));
+    expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.github).toEqual(expect.any(String));
+})
+
+describe("get")
+it("Checks all methods for Engineer class", () => {
+    const engineer = new Engineer("Your engineer", "ab12", "your@engineer.com", "your-engineer");
+    expect(engineer.getName()).toBe(engineer.name);
+    expect(engineer.getId()).toBe(engineer.id);
+    expect(engineer.getEmail()).toBe(engineer.email);
+    expect(engineer.getGithub()).toBe(engineer.github);
+    expect(engineer.getRole()).toBe("Engineer");
+})
