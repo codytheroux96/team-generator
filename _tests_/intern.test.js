d599f0549c7bbefa99d11f8d2fb5de877416c7de
cdt96 <cody.theroux3@gmail.com>
2022-06-18 14:39:50 -0400

    made tests for manager and intern

--- _tests_/intern.test.js
+++ _tests_/intern.test.js
@@ -1,1 +1,20 @@
 const Intern = require("../lib/intern");
+
+describe("Intern")
+it("Creates new intern", () => {
+    const intern = new Intern("Your intern", "ab12", "your@intern.com", "intern-school");
+    expect(intern.name).toBe("Your intern");
+    expect(intern.id).toEqual(expect.any(String));
+    expect(intern.email).toEqual(expect.any(String));
+    expect(intern.school).toEqual(expect.any(String));
+})
+
+describe("get")
+it("Checks all methods for intern class", () => {
+    const intern = new Intern("Your intern", "ab12", "your@intern.com", "intern-school");
+    expect(intern.getName()).toBe(intern.name);
+    expect(intern.getId()).toBe(intern.id);
+    expect(intern.getEmail()).toBe(intern.email);
+    expect(intern.getSchool()).toBe(intern.school);
+    expect(intern.getRole()).toBe("Intern");
+})
--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -1,1 +1,20 @@
 const Manager = require("../lib/manager");
+
+describe("Manager")
+it("Creates new manager", () => {
+    const manager = new Manager("Your manager", "ab12", "your@manager.com", "manager-school");
+    expect(manager.name).toBe("Your manager");
+    expect(manager.id).toEqual(expect.any(String));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.school).toEqual(expect.any(String));
+})
+
+describe("get")
+it("Checks all methods for manager class", () => {
+    const manager = new manager("Your manager", "ab12", "your@manager.com", "1234");
+    expect(manager.getName()).toBe(manager.name);
+    expect(manager.getId()).toBe(manager.id);
+    expect(manager.getEmail()).toBe(manager.email);
+    expect(manager.getOffice()).toBe(manager.office);
+    expect(manager.getRole()).toBe("Manager");
+})
